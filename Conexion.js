fb96775f9505b782380b78f535349bf52d3a57a8
Riverabest22 <edison22rivera@gmail.com>
2019-08-14 20:06:26 -0500

    cambios finales

--- Conexion.js
+++ Conexion.js
@@ -1,15 +1,15 @@
 'use strict'
 
-const $button = document.getElementById('');
+const $button = document.getElementById('button');
 
 $button.addEventListener('click', () => {
     // DOM Elements
-    const $numero = document.getElementById('');
-    const $result = document.getElementById('');
+    const $numero = document.getElementById('number');
+    const $result = document.getElementById('res');
 
     const number = Number.parseInt($numero.value);
     //igual a la funcion que use alejandro
-    const resultado = suma(number);
+    const resultado = contadorDigitos(number);
 
     // imprimir el resultado
     $result.innerHTML = resultado;
