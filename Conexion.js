64246066f9e2a6b06f644cdceeee3a9550b254da
stcristian stiven puenguenan <styven21121@gmail.com>
2019-08-14 19:23:46 -0500

    from js

---
+++ Conexion.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const $button = document.getElementById('');
+
+$button.addEventListener('click', () => {
+    // DOM Elements
+    const $numero = document.getElementById('');
+    const $result = document.getElementById('');
+});
