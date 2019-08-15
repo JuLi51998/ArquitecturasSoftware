f4026cdd0d8ca7305e7dd078d3b9ed547fb71aaa
Riverabest22 <edison22rivera@gmail.com>
2019-08-14 19:33:22 -0500

    re actualizo :v

--- Conexion.js
+++ Conexion.js
@@ -6,4 +6,11 @@ $button.addEventListener('click', () => {
     // DOM Elements
     const $numero = document.getElementById('');
     const $result = document.getElementById('');
+
+    const number = Number.parseInt($numero.value);
+    //igual a la funcion que use alejandro
+    const resultado = suma(number);
+
+
+
 });
